ed34631014024387970024fe9dfb5be6c5cf06e2
jhonatanrojas <jhonatanrojasmd@gmail.com>
2019-04-08 15:39:18 -0400

    p

--- application/models/personasModel.php
+++ application/models/personasModel.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 Class PersonasModel  extends CI_Model{
 
-
+ 
         public $nombre;
         public $apellido;
         public $email;
