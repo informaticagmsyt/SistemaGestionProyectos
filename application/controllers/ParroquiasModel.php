c7473cf25c4eec403c7451fb2e2b51b323491f48
Jhonatan Rojas <35460715+jhonatanrojas@users.noreply.github.com>
2019-04-08 16:00:00 -0400

    Municipios parroquias

---
+++ application/controllers/ParroquiasModel.php
@@ -0,0 +1,63 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+Class ParroquiasModel  extends CI_Model{
+
+
+        
+
+    function __construct(){
+
+   
+        parent::__construct();
+  
+
+    }
+    
+
+    function get($id_municipio){
+    
+      
+        $this->db->select('id_parroquia, id_municipio, parroquia');
+        $this->db->from('parroquias');
+        $this->db->where('id_municipio', $id_municipio);
+          $query = $this->db->get();
+
+       
+        $obj = new stdClass;
+         
+  
+ 
+        $index=array(); 
+        $i=0;      
+        foreach ($query->result() as $row)
+        {
+      
+
+            $obj->response=array(
+                "status"=>"ok",
+                "http_code"=>200
+            );
+
+            $index[$i]["id"]=$row->id_parroquia;
+            $index[$i]["parroquia"]=$row->parroquia;
+
+            
+         
+
+          $i++;
+          $obj->error=array("code"=>"","message"=>"");
+         $obj->comments="Recurso encontrado exitosamente";
+
+        }
+        $obj->data=$index;
+
+
+              return $obj;
+           
+        
+    }
+
+    
+   
+}
