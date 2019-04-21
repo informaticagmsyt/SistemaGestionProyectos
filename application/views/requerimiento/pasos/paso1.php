0f13a58015a0849b17fba56268911196eab733f1
p0nk1 <ponkialva@gmail.com>
2019-04-21 17:14:16 -0400

    arreglos de sintaxis

--- application/views/requerimiento/pasos/paso1.php
+++ application/views/requerimiento/pasos/paso1.php
@@ -1,6 +1,6 @@
 <div class="panel">
   
-  <div id="msj"></div>
+  <div id="msj">
       
     <div class="panel-body">
       
@@ -66,182 +66,182 @@
                   <option value="M">Masculino</option>
                   <option value="F">Femenino</option>
                 </select>
+                <!--END SELECT-->
           </div>
-                    </div>
-                <div class="col-md-4">
-                    <div class="form-group">
-               <label> Fecha de Nacimiento</label>
-               
-           
-           
-           <div class="input-group date form_date col-md-8" data-date="" data-date-format="dd-mm-yyyy" data-link-field="dtp_input2" data-link-format="yyyy-mm-dd">
-               <input class="form-control" size="5" type="text" id="fechanac" name="fecha_nac" value="" >
-               <span class="input-group-addon"><span class="glyphicon glyphicon-remove"></span><i class="far fa-trash-alt"></i></span>
-               <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span><i class="far fa-calendar-alt"></i></span>
-               </div>
-                                             
-           
-           
-               </div>
-               </div>
-            </div>
-                <!---Identificación -->
-
-
-             <!---************************ -->
-                <div class="row">
-                    <div class="col-md-3">
-                        <div class="form-group ">
-                            <label>Telefono</label>
-                        <input type="number" placeholder="telefono" id="telefono"
-                        data-msg-required= "Ingrese un numero de teléfono"
-                         name="telefono" class="form-control" required>
-                      </div>
-                      </div>
-      
-                    <div class="col-md-3">
-                        <div class="form-group ">
-                            <label>Telefono 2</label>
-                        <input type="number" placeholder="telefono 2" id="telefono2"  name="telefono2" class="form-control" >
-                      </div>
-                      </div>
-      
-
-                      <div class="col-md-5">
-                          <div class="form-group ">
-                              <label>Email</label>
-                          <input type="email" 
-                          data-msg-required= "Ingrese un email"
-
-                          placeholder="email@test.com" id="email"  name="email" class="form-control" required>
-                        </div>
-                        </div>
-                  </div>
-                    <!---************************ -->
-
-                    <hr>
-
-                     <!---************************ -->
-                <div class="row">
-                  <div class="col-md-3">
-                      <div class="form-group ">
-                          <label>Profesión / Oficio</label>
-                      <input type="text" placeholder="profesión"
-                       id="profesion" name="profesion" 
-                       class="form-control typeahead" required>
-                    </div>
-                    </div>
-    
-                  <div class="col-md-3">
-                  
-                       
-                          <div class="form-group">
-                            <label>Posee Cartne de la patria</label>
-                        
-                              <select class="custom-select form-control" id="posee_carnet" name="posee_carnet">
-                               
-                                <option value="SI">SI</option>
-                                <option value="NO">NO</option>
-                              </select>
-                            
-                    </div>
-                    </div>
-    
-
-                    <div class="col-md-3">
-                        <div class="form-group ">
-                            <label>Código Carnet de la Patria</label>
-                        <input type="text" placeholder="" id="v_carnet"  name="v_carnet" class="form-control">
-                      </div>
-                      </div>
-
-                      <div class="col-md-3">
-                        <div class="form-group">
-                          <label>Posee Cartne de la patria</label>
-                      
-                            <select class="custom-select form-control" id="v_social" name="v_social">
-                             
-                              <option value="clap">Estructura Clap</option>
-                              <option value="consejo comunal">Consejo Comunal</option>
-                              <option value="jefe de calle">Jefe de Calle</option>
-                              <option value="ffm">FFM</option>
-                              <option value="otro">Otros</option>
-                            </select>
-                          
-                  </div>
-                      </div>
-                </div>
-                  <!---************************ -->
-                  <h4 class="text-center">Ubicación </h4>
-                  <hr>
-
-                              <!---************************ -->
-                <div class="row">
-                
-    
-                  <div class="col-md-4">
-                  
-                       
-                    <div class="form-group">
-                      <label>Estado </label>
-                  
-                        <select class="custom-select form-control" id="estado_id" 
-                        required
-                        name="estado_id">
-                          <option value=""
-                          >Seleccione un estado</option>
-                        </select>
-                      
-              </div>
-                    </div>
-    
-
-                    <div class="col-md-4">
-                      <div class="form-group">
-                        <label>Municipo </label>
-                    
-                          <select 
-                          
-                          class="custom-select form-control" id="municipio_id" name="municipio_id"
-                          required
-                          data-msg-required= "Seleccione un Municipio"
-                          >
-                            <option value="">Seleccione un Municipio</option>
-                          </select>
-                        
-                </div>
-                      </div>
-
-                      <div class="col-md-4">
-                        <div class="form-group">
-                          <label>Parroquia </label>
-                      
-                            <select class="custom-select form-control" id="parroquia_id"
-                            required
-                            name="parroquia_id">
-                              <option value="">Seleccione una parroquia</option>
-                            </select>
-                          
-                  </div>
-                      </div>
-                </div>
-
-                <div class="row">
-                  <div class="col-md-12">
-              
-                      <div class="form-group ">
-                          <label>Dirección de Habitación</label>
-                      <textarea 
-                      id="direccion" 
-                      value=""
-                      data-msg-required= "Ingrese una direccion"
-                       name="direccion" class="form-control" required > </textarea> 
-                  
-
-                  </div>
-                </div>
-          </div>
-
-         </div>
+          <!--END FORM-GROUP-->
         </div>
+        <!--END COL-MD-2-->
+
+        <div class="col-md-4">
+
+          <div class="form-group">
+            <label> Fecha de Nacimiento</label>
+              <div class="input-group date form_date col-md-8" data-date="" data-date-format="dd-mm-yyyy" data-link-field="dtp_input2" data-link-format=" yyyy-mm-dd">
+                <input class="form-control" size="5" type="text" id="fechanac" name="fecha_nac" value="" >
+                <span class="input-group-addon"><span class="glyphicon glyphicon-remove"></span><i class="far fa-trash-alt"></i></span>
+                <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span><i class="far fa-calendar-alt"></i></span>
+              </div>
+              <!--END INPUT-GROUP-->
+          </div>
+          <!--END FORM-GROUP-->
+        </div>
+        <!--END COL-MD-4-->
+      </div>
+      <!--END ROW-->
+
+      <div class="row">
+        
+        <div class="col-md-3">
+          
+          <div class="form-group ">
+            <label>Telefono</label>
+            <input type="number" placeholder="telefono" id="telefono" data-msg-required= "Ingrese un numero de teléfono" name="telefono" class="form-control" required>
+          </div>
+          <!--END FORM-GROUP-->
+        </div>
+        <!--END COL-MD-3-->
+      
+        <div class="col-md-3">
+                       
+          <div class="form-group ">
+            <label>Telefono 2</label>
+            <input type="number" placeholder="telefono 2" id="telefono2"  name="telefono2" class="form-control" >
+          </div>
+          <!--END FORM-GROUP-->
+        </div>
+        <!--END COL-MD-3-->
+        
+        <div class="col-md-5">
+          <div class="form-group ">
+            <label>Email</label>
+            <input type="email" data-msg-required="Ingrese un email" placeholder="email@test.com" id="email"  name="email" class="form-control" required>
+          </div>
+          <!--END FORM-GROUP-->
+        </div>
+        <!--END COL-MD-5-->
+      </div>
+      <!--END ROW-->
+      
+      <hr>
+      
+      <div class="row">
+
+        <div class="col-md-3">
+                      
+          <div class="form-group ">
+            <label>Profesión / Oficio</label>
+            <input type="text" placeholder="profesión" id="profesion" name="profesion" class="form-control typeahead" required>
+          </div>
+          <!--END FORM-GROUP-->
+        </div>
+        <!--END COL-MD-3-->
+
+        <div class="col-md-3">
+                  
+          <div class="form-group">
+            <label>Posee Carnet de la patria</label>
+              <select class="custom-select form-control" id="posee_carnet" name="posee_carnet">
+                <option value="SI">SI</option>
+                <option value="NO">NO</option>
+              </select>
+              <!--END SELECT-->
+          </div>
+          <!--END FORM-GROUP-->
+        </div>
+        <!--END COL-MD-3-->
+        
+        <div class="col-md-3">
+          
+          <div class="form-group ">
+            <label>Código Carnet de la Patria</label>
+              <input type="text" placeholder="" id="v_carnet"  name="v_carnet" class="form-control">
+          </div>
+          <!--END FORM-GROUP-->
+        </div>
+        <!--END COL-MD-3-->
+
+        <div class="col-md-3">
+                        
+          <div class="form-group">
+            <label>Movimiento Social al que pertenece</label>
+              <select class="custom-select form-control" id="v_social" name="v_social">
+                <option value="clap">Estructura Clap</option>
+                <option value="consejo comunal">Consejo Comunal</option>
+                <option value="jefe de calle">Jefe de Calle</option>
+                <option value="ffm">FFM</option>
+                <option value="otro">Otros</option>
+              </select>
+              <!--END SELECT-->
+          </div>
+          <!--END FORM-GROUP-->
+        </div>
+        <!--END COL-MD-3-->
+      </div>
+      <!--END ROW-->
+
+      <h4 class="text-center">Ubicación </h4>
+      
+      <hr>
+
+      <div class="row">
+        
+        <div class="col-md-4">
+          
+          <div class="form-group">
+            <label>Estado</label>
+              <select class="custom-select form-control" id="estado_id" name="estado_id">
+                <option value="">Seleccione un estado</option>
+              </select>
+              <!--END SELECT-->
+          </div>
+          <!--END FORM-GROUP-->
+        </div>
+        <!--END COL-MD-4-->
+        
+        <div class="col-md-4">
+          
+          <div class="form-group">
+            <label>Municipo</label>
+              <select class="custom-select form-control" id="municipio_id" name="municipio_id" data-msg-required="Seleccione un Municipio">
+                <option value="">Seleccione un Municipio</option>
+              </select>
+              <!--END SELECT-->
+          </div>
+          <!--END FORM-GROUP-->
+        </div>
+        <!--END COL-MD-4-->
+        
+        <div class="col-md-4">
+          
+          <div class="form-group">
+            <label>Parroquia</label>
+              <select class="custom-select form-control" id="parroquia_id" name="parroquia_id">
+                <option value="">Seleccione una parroquia</option>
+              </select>
+              <!--END SELECT-->
+          </div>
+          <!--END FORM-GROUP-->
+        </div>
+        <!--END COL-MD-4-->
+      </div>
+      <!--END ROW-->
+
+      <div class="row">
+                  
+        <div class="col-md-12">
+          
+          <div class="form-group ">
+            <label>Dirección de Habitación</label>
+              <textarea id="direccion" value="" data-msg-required="Ingrese una direccion" name="direccion" class="form-control" required> </textarea> 
+          </div>
+          <!--END FORM-GROUP-->
+        </div>
+        <!--END COL-MD-12-->
+      </div>
+      <!--END ROW-->
     </div>
+    <!--END PANEL-BODY-->
+  </div>
+  <!--END MSJ-->
+</div>
+<!--END PANEL-->
