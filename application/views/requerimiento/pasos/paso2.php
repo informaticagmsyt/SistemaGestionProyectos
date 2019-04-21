58ea0394f8483d24dbe38cd7432f865681d85569
p0nk1 <ponkialva@gmail.com>
2019-04-21 18:04:34 -0400

    arreglos de sintaxis

--- application/views/requerimiento/pasos/paso2.php
+++ application/views/requerimiento/pasos/paso2.php
@@ -1,93 +1,90 @@
-
-    <div class="panel">
-
+<div class="panel">
   
+  <div id="msj2">
+    
+    <div class="panel-body">
   
-        <div id="msj2"></div>
-        
-          <div class="panel-body">
+      <h5 class="text-center">Datos del Requerimiento</h5>
+                    
+      <hr>
   
-          <h5 class="text-center">Datos del Requerimiento</h5>
-                    <hr>
-  
-                    <div class="row">
-                      <div class="col-md-6">
-                          <div class="form-group ">
-                              <label>Nombre del Requerimiento</label>
-                          <input type="text" placeholder="nombrereq" id="nombrereq"
-                          data-msg-required= "Ingrese un nombre"
-                           name="nombrereq" class="form-control" required>
-                        </div>
-                        </div>
-        
-                      <div class="col-md-6">
-                     
-                        <div class="form-group">
-                          <label>Estatus del Requerimiento </label>
+      <div class="row">
                       
-                            <select class="custom-select form-control" 
-                            id="estatus_requerimiento_id" 
-                            name="estatus_requerimiento_id">
-                              <option value="0">Seleccione un opcion</option>
-                            </select>
+        <div class="col-md-6">
                           
-                        </div>
-        
-  
-                        
-                    </div>
+          <div class="form-group ">
+            <label>Nombre del Requerimiento</label>
+              <input type="text" placeholder="nombrereq" id="nombrereq" data-msg-required= "Ingrese un nombre" name="nombrereq" class="form-control" required>
+          </div>
+          <!--END FORM-GROUP-->
         </div>
-  
+        <!--END COL-MD-6-->
+        
+        <div class="col-md-6">
 
-
+          <div class="form-group">
+            <label>Estatus del Requerimiento </label>
+              <select class="custom-select form-control" id="estatus_requerimiento_id" name="estatus_requerimiento_id">
+                <option value="0">Seleccione un opcion</option>
+              </select>
+          </div>
+          <!--END FORM-GROUP-->
+        </div>
+        <!--END COL-MD-6-->
+      </div>
+      <!--END ROW-->
   
-        <div class="row">
-          <div class="col-md-6">
-              <div class="form-group ">
-                  <label>Categoria</label>
-  
-                  <select class="custom-select form-control" 
-                  id="categoria_id" 
-                  name="categoria_id"              required>
-                    <option value="">Seleccione un opcion</option>
-                  </select>
-            </div>
-            </div>
-  
-          <div class="col-md-6">
-         
-            <div class="form-group">
-              <label>Sub Categoria </label>
+      <div class="row">
+        
+        <div class="col-md-6">
           
-                <select class="custom-select form-control" 
-                id="sub_categoria_id" 
-                name="sub_categoria_id"              required>
-                  <option value="">Seleccione un opcion</option>
-                </select>
-            </div>
+          <div class="form-group ">
+            <label>Categoria</label>
+              <select class="custom-select form-control" id="categoria_id" name="categoria_id" required>
+                <option value="">Seleccione un opcion</option>
+              </select>
+              <!--END SELECT-->
+          </div>
+          <!--END FORM-GROUP-->
+        </div>
+        <!--END COL-MD-6-->
+        
+        <div class="col-md-6">
           
-          </div>  
-
-  <div class="row">
-    <hr>
-    <div class="col-md-12">
-        <div class="form-group ">
+          <div class="form-group">
+            <label>Sub Categoria </label>
+              <select class="custom-select form-control" id="sub_categoria_id" name="sub_categoria_id" required>
+                <option value="">Seleccione un opcion</option>
+              </select>
+              <!--END SELECT-->
+          </div>
+          <!--END FORM-GROUP-->
+        </div>
+        <!--END COL-MD-6-->
+      </div>
+      <!--END ROW-->
+      
+      <hr>
+        
+      <div class="row">
+        
+        <div class="col-md-12">
+        
+          <div class="form-group ">
             <label>Descripcion del Proyecto</label>
-  
-            <textarea 
-            id="descripcion" 
-            value=""
-            data-msg-required= "Ingrese una descripcion"
-             name="descripcion" class="form-control" required > </textarea> 
+              <textarea id="descripcion" value="" data-msg-required= "Ingrese una descripcion" name="descripcion" class="form-control" required > </textarea> 
+          </div>
+          <!--END FORM-GROUP-->
       </div>
-      </div>
+      <!--END ROW-->
   
       <input type="hidden"  id="cedula2"  name="cedula2"  >
       <input type="hidden"  id="cedula2"  name="cedula2"  >
       <input type="hidden"  id="idrequerimiento"  name="idrequerimiento"  >
       <input type="hidden"  id="idproyecto"  name="idrequerimiento"  >
-  </div>
-        </div>
-  
-      
     </div>
+    <!--END PANEL-BODY-->
+  </div>
+  <!--END MSJ2-->
+</div>
+<!--END PANEL-->
