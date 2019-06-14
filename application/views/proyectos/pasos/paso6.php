b7d0c82b1dd028f474bb3abac825a014b2510060
p0nk1 <ponkialva@gmail.com>
2019-06-14 16:22:29 -0400

    agregado paso6 (Mano de Obra e Inversion Total) agregar lo especificado en hoja 1 y 2

---
+++ application/views/proyectos/pasos/paso6.php
@@ -0,0 +1,18 @@
+<div class="panel">
+  
+  
+  
+        <div id="msj6"></div>
+        
+          <div class="panel-body">
+          <!-- INSERTE CODIGO DESDE AQUI -->                
+          </div>
+</div>
+  <script>
+//* 
+
+INSERTE SCRIPTS AQUI
+
+/**/       
+  </script>
+  
