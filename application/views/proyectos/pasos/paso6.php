c07b3b1e0923e08d4fe07482f5da64fe3d064945
p0nk1 <ponkialva@gmail.com>
2019-06-17 16:58:48 -0400

    vista completada

--- application/views/proyectos/pasos/paso6.php
+++ application/views/proyectos/pasos/paso6.php
@@ -32,22 +32,72 @@
 		          		<div class="input-group">
 	  						<span class="input-group-addon">Bs</span>
 	  						<input class="form-control" id="salariominimo" name="salariominimo" value="65000" type="number" readonly>
-	  						<span class="input-group-addon">.00</span>
 						</div>
 						<br>
 						<div class="input-group">
 	  						<span class="input-group-addon">Bs</span>
 	  						<input class="form-control" id="salariominimo" name="salariominimo" value="65000" type="number" readonly>
-	  						<span class="input-group-addon">.00</span>
 						</div>
 						<br>
 						<div class="input-group">
 	  						<span class="input-group-addon">Bs</span>
 	  						<input class="form-control" id="salariominimo" name="salariominimo" value="65000" type="number" readonly>
-	  						<span class="input-group-addon">.00</span>
 						</div>
 					</div>
 
+	          	</div>
+
+	          	<h5 class="text-center"> Estructura de Costos </h5>
+	          	<hr>
+
+	          	<div class="row">
+	          		<div class="col-md-3">
+	          			<label for="insumos" class="control-label"> Materia Prima e Insumos </label>
+	          			<div class="input-group">
+	          				<span class="input-group-addon">Bs</span>
+	          				<input class="form-control" type="number" name="costoInsumo" readonly><!-- Aqui va la sumatoria de la cantidad mas el precio de los insumos en el paso 5 -->
+	          			</div>	
+	          		</div>
+
+	          		<div class="col-md-3">
+	          			<label for="herramientas" class="control-label"> Herramientas y Equipos de Trabajo </label>
+	          			<div class="input-group">
+	          				<span class="input-group-addon">Bs</span>
+	          				<input class="form-control" type="number" name="costoHerramientas" readonly><!-- Aqui va la sumatoria de la cantidad mas el precio de las herramientas de trabajo en el paso 5 -->
+	          			</div>	
+	          		</div>
+
+	          		<div class="col-md-3">
+	          			<label for="maquinas" class="control-label"> Maquinas y Equipos Técnologicos </label>
+	          			<div class="input-group">
+	          				<span class="input-group-addon">Bs</span>
+	          				<input class="form-control" type="number" name="costoMaquinas" readonly><!-- Aqui va la sumatoria de la cantidad mas el precio de los maquinas tecnologicas en el paso 5 -->
+	          			</div>	
+	          		</div>
+
+	          		<div class="col-md-4">
+	          			<label for="mobiliario" class="control-label"> Mobiliario y Equipos Complementarios </label>
+	          			<div class="input-group">
+	          				<span class="input-group-addon">Bs</span>
+	          				<input class="form-control" type="number" name="costoMobiliario" readonly><!-- Aqui va la sumatoria de la cantidad mas el precio de los mobiliario en el paso 5 -->
+	          			</div>	
+	          		</div>
+
+	          		<div class="col-md-3">
+	          			<label for="manodeobra" class="control-label"> Mano de Obra </label>
+	          			<div class="input-group">
+	          				<span class="input-group-addon">Bs</span>
+	          				<input class="form-control" type="number" name="costoManodeobra" readonly><!-- Aqui va la sumatoria de la cantidad mas el precio de la mano de obra en el paso 6 -->
+	          			</div>	
+	          		</div>
+
+	          		<div class="col-md-3">
+	          			<label for="servicios" class="control-label"> Servicios Públicos </label>
+	          			<div class="input-group">
+	          				<span class="input-group-addon">Bs</span>
+	          				<input class="form-control" type="number" name="costoServicio" ><!-- Aqui va la sumatoria de la cantidad mas el precio de los servicios en el paso 4 -->
+	          			</div>	
+	          		</div>
 	          	</div>
 
 	        </div>
