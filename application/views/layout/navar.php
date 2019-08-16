279884e012e64d0727064a84af8d1593f9156f25
p0nk1 <ponkialva@gmail.com>
2019-08-16 14:49:23 -0400

    los creadores

--- application/views/layout/navar.php
+++ application/views/layout/navar.php
@@ -6,7 +6,7 @@
           <span class="px-demo-logo bg-primary">INICIO</span>
               <span class="px-demo-logo-1">
 
-            </span> - Sistema de Gestión de Proyectos Socio-Productivos</a>
+            </span> - S.G.P.S.P</a>
     </div>
     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#px-navbar-collapse" aria-expanded="false"><i class="navbar-toggle-icon"></i></button>
 
@@ -17,7 +17,7 @@
 
       <ul class="nav navbar-nav navbar-right">
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+          <a href="#63 72 65 61 64 6f 20 70 6f 72 20 70 30 6e 6b 31 2c 61 6c 65 78 31 38 31 32 72 20 79 20 6a 68 6f 6e 61 74 61 6e" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
             <?php echo $nombreUser?>
           </a>
           <!--ul class="dropdown-menu">
