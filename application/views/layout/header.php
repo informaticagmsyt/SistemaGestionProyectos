ad8ea36684c74423e324ab6689e9e676e724568b
p0nk1 <ponkialva@gmail.com>
2019-10-11 13:44:40 -0400

    mejoras menores

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
 
-  <title>Sistema Gestion Proyectos</title>
+  <title>S.G.P.S.P</title>
 
   <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,600,700,300&subset=latin" rel="stylesheet" type="text/css">
   <link href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet" type="text/css">
