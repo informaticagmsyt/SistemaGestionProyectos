abb62435f40b8b297b736355e6c514dc627baf61
p0nk1 <ponkialva@gmail.com>
2019-11-19 13:00:48 -0400

    Modulos a terminar

--- application/views/layout/nav.php
+++ application/views/layout/nav.php
@@ -8,7 +8,7 @@
 
     <ul class="px-nav-content">
 
-    <!--li class="px-nav-item px-nav-dropdown active">
+    <li class="px-nav-item px-nav-dropdown active">
         <a href="#"><i class="px-nav-icon far fa-address-card"></i><span class="px-nav-label">Requerimiento</span></a>
 
         <ul class="px-nav-dropdown-menu">
@@ -16,7 +16,7 @@
           <li class="px-nav-item"><a href="#"><i class="px-nav-icon fa fa-search"></i><span class="px-nav-label">Consultar</span></a></li>
          
         </ul>
-      </li-->
+      </li>
 
       <li class="px-nav-item px-nav-dropdown ">
         <a href="#"><i class="px-nav-icon fas fa-book"></i><span class="px-nav-label">Proyectos</span></a>
@@ -41,13 +41,13 @@
  
 
       
-      <!--li class="px-nav-item px-nav-dropdown ">
+      <li class="px-nav-item px-nav-dropdown ">
         <a href="#"><i class="px-nav-icon fas fa-clipboard-list " ></i><span class="px-nav-label">Tareas</span></a>
         <ul class="px-nav-dropdown-menu">
           <li class="px-nav-item"><a href="<?php echo base_url();?>tareas"><i class="px-nav-icon fas fa-plus"></i><span class="px-nav-label">Nueva Tarea </span></a></li>
           <li class="px-nav-item"><a href="#"><i class="px-nav-icon fa fa-search"></i><span class="px-nav-label">Listar Tareas</span></a></li>
         </ul>
-      </li-->
+      </li>
 
 
     
